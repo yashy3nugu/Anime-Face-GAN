85a99c11e99a2321ad1a264b02a52ddfc63576fe
Yash <yashyenugu@gmail.com>
2020-12-16 23:03:58 +0530

    Added code to save images per epoch
    Evolution of images by the generator can be seen every epoch

---
+++ utils.py
@@ -0,0 +1,43 @@
+import numpy as np
+import os
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow import keras
+
+class Save_images(keras.callbacks.Callback):
+
+    def __init__(self,noise,PREVIEW_MARGIN,PREVIEW_ROWS,PREVIEW_COLS,generator,**kwargs):
+        super(keras.callbacks.Callback,self).__init__(**kwargs)
+        self.noise = noise
+        self.preview_margin = PREVIEW_MARGIN
+        self.preview_rows = PREVIEW_ROWS
+        self.preview_cols = PREVIEW_COLS
+        self.generator = generator
+
+    def on_epoch_end(self, epoch, logs=None):
+        image_array = np.full((
+            self.preview_margin + (self.preview_rows * (64 + self.preview_margin)),
+            self.preview_margin + (self.preview_cols * (64 + self.preview_margin)), 3),
+            255, dtype=np.uint8)
+
+        generated_images = self.generator.predict(self.noise)
+
+        generated_images = 0.5 * generated_images + 0.5
+
+        image_count = 0
+        for row in range(self.preview_rows):
+            for col in range(self.preview_cols):
+                r = row * (64 + 16) + self.preview_margin
+                c = col * (64 + 16) + self.preview_margin
+                image_array[r:r + 64, c:c + 64] = generated_images[image_count] * 255
+                image_count += 1
+
+        output_path = 'Epoch_images'
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+
+        filename = os.path.join(output_path, f"train-{epoch}.png")
+        im = Image.fromarray(image_array)
+        im.save(filename)
