1fac3eb842b2d5ff056d79519b3cfa21af869ffe
Yash <yashyenugu@gmail.com>
2021-01-18 12:23:03 +0530

    Refactored variable names for easier readability

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -12,32 +12,35 @@ class save_images(keras.callbacks.Callback):
     On subclassing it we can specify methods which can be executed while training
     """
 
-    def __init__(self,noise,preview_margin,preview_rows,preview_cols,**kwargs):
+    def __init__(self,noise,margin,num_rows,num_cols,**kwargs):
         super(keras.callbacks.Callback,self).__init__(**kwargs)
         self.noise = noise
-        self.preview_margin = preview_margin
-        self.preview_rows = preview_rows
-        self.preview_cols = preview_cols
+        self.margin = margin
+        self.num_rows = num_rows
+        self.num_cols = num_cols
 
     # overwriting on_epoch_end() helps in executing a custom method when an epoch ends
     def on_epoch_end(self, epoch, logs=None):
         """
-               :param cnt: integer, specifying the epoch number
-               :param noise: input noise vector from a normal distribution for the generator to generate images
-               :param PREVIEW_MARGIN: int, insert margin between images
-               :param PREVIEW_ROWS: int, specify number of rows of images
-               :param PREVIEW_COLS: int, specify number of columns of images
-               :param generator: neural network model to generate images
-               :return: image containing a grid of PREVIEW_ROWS*PREVIEW_COLS images to check generator progress
+        
+        Input:
+            noise: fixed noise vector from a normal distribution to be fed to the generator.
+            num_rows: int, specify number of rows of images
+            num_cols: int, specify number of columns of images
+            margin: int, specify margin between images
+            generator: keras model representing the generator network
+        
+        Output:
+            image containing a grid of num_rows*num_cols images to check generator progress
         """
 
         # Generate a base array upon which images can then be added sequentially
         image_array = np.full((
-            self.preview_margin + (self.preview_rows * (64 + self.preview_margin)),
-            self.preview_margin + (self.preview_cols * (64 + self.preview_margin)), 3),
+            self.margin + (self.num_rows * (64 + self.margin)),
+            self.margin + (self.num_cols * (64 + self.margin)), 3),
             255, dtype=np.uint8)
 
-        # Generate PREVIEW_ROWS*PREVIEW_COLS number of images using the generator model
+        # Generate num_rows*num_cols number of images using the generator model
         generated_images = self.model.generator.predict(self.noise)
         generated_images = 0.5 * generated_images + 0.5 # Convert pixel intensities to the range [0,1]
         """The images need not be converted into the typical [0,255] pixel intensity values because
@@ -46,10 +49,10 @@ class save_images(keras.callbacks.Callback):
 
 
         image_count = 0
-        for row in range(self.preview_rows):
-            for col in range(self.preview_cols):
-                r = row * (64 + 16) + self.preview_margin
-                c = col * (64 + 16) + self.preview_margin
+        for row in range(self.num_rows):
+            for col in range(self.num_cols):
+                r = row * (64 + 16) + self.margin
+                c = col * (64 + 16) + self.margin
                 image_array[r:r + 64, c:c + 64] = generated_images[image_count] * 255
                 image_count += 1
 
@@ -69,5 +72,5 @@ class checkpoint_callback(keras.callbacks.Callback):
     def __init__(self,**kwargs):
         super(keras.callbacks.Callback, self).__init__(**kwargs)
     def on_epoch_end(self, epoch, logs=None):
-        self.model.generator.save_weights("Weights/generator_weights.h5")
-        self.model.discriminator.save_weights("Weights/discriminator_weights.h5")
+        self.model.generator.save_weights("Weights/generator_weights-test.h5")
+        self.model.discriminator.save_weights("Weights/discriminator_weights-test.h5")
